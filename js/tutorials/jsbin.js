4b865816ded2bf3656d3e617c373c9073f232046
Michael Brooks <mjbrooks@uw.edu>
2014-11-06 19:38:28 -0800

    move www to dist

---
+++ js/tutorials/jsbin.js
@@ -0,0 +1,61 @@
+(function ($) {
+
+  var loadJSBin = function (button, panel) {
+
+    var link = button.attr('href').replace('/edit', '/embed');
+    if (link.indexOf('height=') < 0) {
+      link += '&height=100%';
+    }
+
+    panel.html($('<a class="jsbin-embed">').attr('href', link));
+    readJSBinLinks();
+
+    //Override the height setting
+    panel.find('iframe').css({
+      'height': '100%',
+      'border': 'none'
+    });
+  };
+
+  var setupSplitter = function() {
+    $('.tutorial .split-panel').split({
+      orientation: 'vertical',
+      limit: 10,
+      position: '40%' // if there is no percentage it interpret it as pixels
+    });
+  };
+
+  var setupNavMenu = function() {
+    var tutorial = $('.tutorial');
+    var links = tutorial.find('h1,h2');
+    //TODO: build a nav menu
+  };
+
+  $(document).ready(function () {
+
+    setupSplitter();
+    setupNavMenu();
+
+    var jsbin = $('.jsbin-container');
+    var buttons = $('a.jsbin-button');
+
+    buttons.click(function (e) {
+      e.preventDefault();
+      if (confirm("Are you sure you want to load this JS Bin?\nThis will override any edits you have made.")) {
+        loadJSBin($(this), jsbin);
+      }
+      return false;
+    });
+
+    buttons.each(function() {
+      $(this).append('&nbsp;<i class="glyphicon glyphicon-share-alt">')
+    });
+
+    loadJSBin(buttons.first(), jsbin);
+  });
+
+  if (typeof(hljs) !== 'undefined') {
+    hljs.initHighlightingOnLoad();
+  }
+
+})(jQuery);
