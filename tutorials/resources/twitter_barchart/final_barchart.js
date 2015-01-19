27bb9eb4bd4aa468e0c1719a89192102bc996dba
Michael Brooks <mjbrooks@uw.edu>
2015-01-19 15:49:25 -0800

    update dist

--- tutorials/resources/twitter_barchart/final_barchart.js
+++ tutorials/resources/twitter_barchart/final_barchart.js
@@ -1,36 +1,37 @@
 (function () {
-  var data = d3.select('#cars').html().trim();
+  var data = d3.select('#top_20_hashtags').html().trim();
   data = d3.csv.parse(data, function (d) {
     return {
-      model: d.Model,
-      origin: d.Origin,
-      year: +d.Year,
-      cylinders: +d.Cylinders,
-      horsepower: +d.Horsepower,
-      mpg: +d.MPG,
-      weight: +d.Weight
+      hashtag: d.hashtag,
+      num_convs: +d.num_convs,
+      num_tweets: +d.num_tweets,
+      num_users: +d.num_users
     };
   });
 
-// Create the SVG element
+//Add an SVG element
   var width = 500;
   var height = 400;
-
   var svg = d3.select('body')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
-// Calculate the max weight and car model names
-  var max_weight = d3.max(data, function(d) {
-    return d.weight;
+
+//Determine data ranges
+  var max_tweets = d3.max(data, function (d) {
+    return d.num_tweets;
   });
 
-  var model_names = data.map(function(d){
-    return d.model;
+  var min_tweets = d3.min(data, function (d) {
+    return d.num_tweets;
   });
 
-// Define the margins
+  var tag_names = data.map(function (d) {
+    return d.hashtag;
+  });
+
+//Add margins
   var margins = {
     top: 20,
     right: 20,
@@ -40,20 +41,20 @@
   var graphWidth = width - margins.right - margins.left;
   var graphHeight = height - margins.top - margins.bottom;
 
-// Make a group for drawing the chart
   var chart = svg.append('g')
     .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
 
-// Create the scales
+
+//Create scales
   var x = d3.scale.linear()
     .range([0, graphWidth])
-    .domain([0, max_weight]);
+    .domain([0, max_tweets]);
 
   var y = d3.scale.ordinal()
-    .domain(model_names)
-    .rangeRoundBands([ 0, graphHeight], 0.1);
+    .domain(tag_names)
+    .rangeRoundBands([0, graphHeight], 0.1);
 
-// Draw the axes
+//Draw the axes
   var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top");
@@ -70,7 +71,7 @@
     .classed("y axis", true)
     .call(yAxis);
 
-// Draw the bars
+//Draw the bars
   var bars = chart.selectAll('rect.bar')
     .data(data);
 
@@ -78,15 +79,16 @@
     .append('rect')
     .attr('class', 'bar');
 
+
   bars.exit()
     .remove();
 
   bars.attr('x', 0)
-    .attr('y', function(d) {
-      return y(d.model);
+    .attr('y', function (d) {
+      return y(d.hashtag);
     })
     .attr('height', y.rangeBand())
-    .attr('width', function(d) {
-      return x(d.weight);
+    .attr('width', function (d) {
+      return x(d.num_tweets);
     });
 })();
